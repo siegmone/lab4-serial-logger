1f98f2392bdb87b95adffc49f24c9ea27e736892
Simone Vortice <simone.vortice@proton.me>
2024-11-16 17:03:05 +0100

    added comment to run-acquisitions.sh

--- run-acquisitions.sh
+++ run-acquisitions.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-python3 main.py 170 -p /dev/ttyUSB1 -s 400 \
-    && echo "\nAcquisizione finita, andate in pace"
+# Automatically run consecutive tasks with custom setting
+
+./main.py 6 -p /dev/ttyUSB1 -s 400 && echo "\nAcquisizione finita"
